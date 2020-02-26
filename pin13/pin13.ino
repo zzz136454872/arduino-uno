63dfdb0d77653fa0903681e81a7688eff431a294
f4prime <zzz136454872@163.com>
2020-02-26 15:44:14 +0800

    two new

---
+++ pin13/pin13.ino
@@ -0,0 +1,17 @@
+/* @author f4prime
+ * @email zzz136454872@163.com
+ * @function test if the led marked as L is
+ * linked to digital pin 13
+ */
+
+
+void setup() {
+    Serial.begin(9600);
+    Serial.print("start");
+
+    
+}
+
+void loop() {
+    
+}
