8881a18894bf85badb022e558bfce0478f04b259
f4prime <zzz136454872@163.com>
2020-02-27 20:57:48 +0800

    add 1

---
+++ setup/setup.ino
@@ -0,0 +1,19 @@
+/* @author f4prime
+ * @email zzz136454872@163.com
+ */
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("start");
+
+    
+}
+
+void loop() {
+    
+}
+
+// result:
+// the button in the corner of uno r3 can restart the board and
+// run setup again. 
+// the small button in the middle of nano can do the same thing. 
